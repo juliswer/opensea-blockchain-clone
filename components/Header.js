6beb726a3165bd3eca433841aa4009fc61d7ea8b
juliswer <julianswer36@gmail.com>
2022-05-08 15:26:54 -0300

    uploading the updates from the header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import openseaLogo from "../assets/opensea.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineAccountBalanceWallet } from "react-icons/md";
 
 const style = {
   wrapper: `bg-[#04111d] w-screen px-[1.2rem] py-[0.8rem] flex `,
@@ -24,6 +26,27 @@ const Header = () => {
           <div className={style.logoText}>Opensea</div>
         </div>
       </Link>
+      <div className={style.searchBar}>
+        <div className={style.searchIcon}>
+          <AiOutlineSearch />
+        </div>
+        <input
+          className={style.searchInput}
+          placeholder="Search items, collections and accounts"
+        />
+      </div>
+      <div className={style.headerItems}>
+        <div className={style.headerItem}>Colections</div>
+        <div className={style.headerItem}>Stats</div>
+        <div className={style.headerItem}>Resources</div>
+        <div className={style.headerItem}>Create</div>
+        <div className={style.headerIcon}>
+            <CgProfile />
+        </div>
+        <div className={style.headerIcon}>
+            <MdOutlineAccountBalanceWallet />
+        </div>
+      </div>
     </div>
   );
 };
