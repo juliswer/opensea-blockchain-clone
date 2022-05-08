db182e23bbe0750de8c6362c9687760aedb6fd83
juliswer <julianswer36@gmail.com>
2022-05-08 16:51:11 -0300

    hero content upload

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
