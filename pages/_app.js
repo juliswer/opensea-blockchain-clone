305712626012b60a075807548024b999137f9025
juliswer <julianswer36@gmail.com>
2022-05-09 21:05:14 -0300

    uploading metamask ext

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 
+// ? Chain ID 4 === Rinkeby Testing Network
+// ? Injected is a connector used by metamask
+
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebWeb3Provider
+        supportedChainIds={supportedChainIds}
+        connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import { useWeb3 } from "@3rdweb/hooks";
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+};
 
 export default function Home() {
+  const { address, connectWallte } = useWeb3();
   return (
-    <>
-      <Header />
-      <Hero />
-    </>
+    <div className={style.wrapper}>
+      {address ? (
+        <>
+          <Header />
+          <Hero />
+        </>
+      ) : (
+        <button
+          className={style.button}
+          onClick={() => connectWallte('injected')}
+        >
+          Connect Wallter
+        </button>
+      )}
+    </div>
   );
 }
