b4ac90bfd7670df1d7f47c4139db2a6f99b787e1
juliswer <julianswer36@gmail.com>
2022-05-11 15:31:10 -0300

    uploading nfts

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { useWeb3 } from "@3rdweb/hooks";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+const style = {
+  bannerImageContainer: `h-[20vh] w-screen overflow-hidden flex justify-center items-center`,
+  bannerImage: `w-full object-cover`,
+  infoContainer: `w-screen px-4`,
+  midRow: `w-full flex justify-center text-white`,
+  endRow: `w-full flex justify-end text-white`,
+  profileImg: `w-40 h-40 object-cover rounded-full border-2 border-[#202225] mt-[-4rem]`,
+  socialIconsContainer: `flex text-3xl mb-[-2rem]`,
+  socialIconsWrapper: `w-44`,
+  socialIconsContent: `flex container justify-between text-[1.4rem] border-2 rounded-lg px-2`,
+  socialIcon: `my-2`,
+  divider: `border-r-2`,
+  title: `text-5xl font-bold mb-4`,
+  createdBy: `text-lg mb-4`,
+  statsContainer: `w-[44vw] flex justify-between py-4 border border-[#151b22] rounded-xl mb-4`,
+  collectionStat: `w-1/4`,
+  statValue: `text-3xl font-bold w-full flex items-center justify-center`,
+  ethLogo: `h-6 mr-2`,
+  statName: `text-lg w-full text-center mt-1`,
+  description: `text-[#8a939b] text-xl w-max-1/4 flex-wrap mt-4`,
+};
 
 const Collection = () => {
-  const { query } = useRouter();
+  const router = useRouter();
+  const { provider } = useWeb3();
+  const { collectionId } = router.query;
+  const [nfts, setNfts] = useState([]);
+  const [listings, setListings] = useState([]);
+
+  const ALCHEMY_KEY_HTTPS = process.env.NEXT_PUBLIC_ALCHEMY_KEY_HTTPS;
+  console.log(ALCHEMY_KEY_HTTPS);
+
+  const nftModule = useMemo(() => {
+    const sdk = new ThirdwebSDK(provider.getSigner());
+  });
+
   return (
     <Link href="/">
-      <h2>Great, the collection is: {query.collectionId}</h2>
+      <h2>Great, the collection is: {collectionId}</h2>
     </Link>
   );
 };
